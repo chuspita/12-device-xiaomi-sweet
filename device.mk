01b6329e48b014affb8e87aaea7c2b508fa7b990
chuspita <kosecki99@gmail.com>
2022-09-01 18:08:21 +0200

    sweet: sparkify

--- device.mk
+++ device.mk
@@ -90,7 +90,7 @@ PRODUCT_COPY_FILES += \
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-evolution
+    $(LOCAL_PATH)/overlay-spark
 
 # Overlay-RRO
 PRODUCT_PACKAGES += \
